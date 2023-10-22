016e602fd778105c84c117dcdc1e5290f7e46cb4
Riccardo Mazzarini <me@noib3.dev>
2023-10-22 17:42:47 +0200

    add first test for `FzfV2`

--- tests/fzf_v2.rs
+++ tests/fzf_v2.rs
@@ -1,1 +1,29 @@
+use norm::fzf::{bonus, penalty, FzfQuery, FzfV2};
+use norm::{CaseSensitivity, Metric};
 
+#[test]
+fn fzf_v2_empty_query() {
+    let fzf = FzfV2::new();
+    let empty = FzfQuery::new("");
+    assert!(fzf.distance(empty, "foo").is_none());
+}
+
+#[test]
+fn fzf_v2_score_1() {
+    let fzf = FzfV2::new()
+        .with_case_sensitivity(CaseSensitivity::Insensitive)
+        .with_matched_ranges(true);
+
+    let query = FzfQuery::new("oBZ");
+
+    let mach = fzf.distance(query, "fooBarbaz").unwrap();
+
+    assert_eq!(
+        mach.distance().into_score(),
+        bonus::MATCH * 3 + bonus::CAMEL_123
+            - penalty::GAP_START
+            - penalty::GAP_EXTENSION * 3
+    );
+
+    // assert_eq!(mach.matched_ranges(), [2..4, 8..9]);
+}
