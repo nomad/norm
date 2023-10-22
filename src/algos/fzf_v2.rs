2893bfe684a4cfdcae1cc1a6dc41f42042162c43
Riccardo Mazzarini <me@noib3.dev>
2023-10-22 17:42:36 +0200

    fix bugs

--- src/algos/fzf_v2.rs
+++ src/algos/fzf_v2.rs
@@ -1,5 +1,3 @@
-use core::ops::Range;
-
 use super::fzf_v1::*;
 use crate::{CaseSensitivity, Match, Metric};
 
@@ -85,8 +83,6 @@ impl FzfV2 {
 
                 if let Some(next_char) = query_chars.next() {
                     query_char = next_char;
-                } else {
-                    break;
                 }
             }
 
@@ -129,114 +125,29 @@ impl FzfV2 {
             return Some(max_score);
         }
 
+        // println!("f: {f:?}");
+        // println!("t: {t:?}");
+        // println!("b: {b:?}");
+        // println!("h0: {h0:?}");
+        // println!("c0: {c0:?}");
+        // println!("last_idx: {last_idx:?}");
+
         // Phase 3.
         //
         // Fill in score matrix H.
 
-        let f0 = f[0];
+        let score = phase_3(
+            &mut f,
+            &t,
+            &b,
+            &h0,
+            &c0,
+            query,
+            last_idx,
+            self.case_sensitivity,
+        );
 
-        let width = last_idx - f0 + 1;
-
-        let mut h = vec![0; m * width];
-
-        h[f0..last_idx + 1].copy_from_slice(&h0[f0..last_idx + 1]);
-
-        // Possible length of consecutive chunk at each position.
-        let mut c = vec![0; m * width];
-
-        c[f0..last_idx + 1].copy_from_slice(&c0[f0..last_idx + 1]);
-
-        let f_sub = &mut f[1..];
-
-        let p_sub = &query[1..][..f_sub.len()];
-
-        for (offset, f) in f_sub.iter().enumerate() {
-            let c_len = c.len();
-            let c_ptr = c.as_mut_ptr();
-
-            let h_len = h.len();
-            let h_ptr = h.as_mut_ptr();
-
-            let pchar = p_sub.chars().nth(offset).unwrap();
-            let pidx = offset + 1;
-            let row = pidx * width;
-            let mut is_in_gap = false;
-            let t_sub = &t[*f..last_idx + 1];
-            let b_sub = &b[*f..][..t_sub.len()];
-            let c_sub = &mut c[row + f - f0..][..t_sub.len()];
-            let h_sub = &mut h[row + f - f0..][..t_sub.len()];
-
-            let c_diag = {
-                let c =
-                    unsafe { core::slice::from_raw_parts_mut(c_ptr, c_len) };
-                &mut c[row + f - f0 - 1 - width..][..t_sub.len()]
-            };
-
-            let h_diag = {
-                let h =
-                    unsafe { core::slice::from_raw_parts_mut(h_ptr, h_len) };
-                &mut h[row + f - f0 - 1 - width..][..t_sub.len()]
-            };
-
-            let h_left = {
-                let h =
-                    unsafe { core::slice::from_raw_parts_mut(h_ptr, h_len) };
-                &mut h[row + f - f0 - 1..][..t_sub.len()]
-            };
-
-            for (offset, &char) in t_sub.iter().enumerate() {
-                let col = offset + f;
-
-                let mut s1 = 0;
-
-                let mut consecutive = 0;
-
-                let s2 = if is_in_gap {
-                    h_left[offset].saturating_sub(penalty::GAP_EXTENSION)
-                } else {
-                    h_left[offset].saturating_sub(penalty::GAP_START)
-                };
-
-                if pchar == char {
-                    s1 = h_diag[offset] + bonus::MATCH;
-                    let mut small_b = b_sub[offset];
-                    consecutive = c_diag[offset] + 1;
-
-                    if consecutive > 1 {
-                        let fb = b[col - consecutive + 1];
-                        if small_b >= bonus::BOUNDARY && small_b > fb {
-                            consecutive = 1;
-                        } else {
-                            small_b = bonus::CONSECUTIVE.max(fb).max(small_b);
-                        }
-                    }
-
-                    if s1 + small_b < s2 {
-                        s1 += b_sub[offset];
-                        consecutive = 0;
-                    } else {
-                        s1 += small_b;
-                    }
-                }
-
-                c_sub[offset] = consecutive;
-
-                is_in_gap = s1 < s2;
-
-                let score = s1.max(s2).max(0);
-
-                if pidx == m - 1 && score > max_score {
-                    max_score = score;
-                    max_score_pos = col;
-                }
-
-                h_sub[offset] = score;
-            }
-        }
-
-        let j = f0;
-
-        Some(max_score)
+        Some(score)
     }
 
     /// TODO: docs
@@ -295,4 +206,159 @@ impl Metric for FzfV2 {
 
         Some(Match::new(distance, Vec::new()))
     }
+}
+
+fn phase_2() {}
+
+fn phase_3(
+    f: &mut [usize],
+    t: &[char],
+    b: &[Score],
+    h0: &[Score],
+    c0: &[usize],
+    query: &str,
+    last_idx: usize,
+    case_sensitivity: CaseSensitivity,
+) -> Score {
+    let m = query.len();
+
+    let mut max_score = 0;
+
+    let mut max_score_pos = 0;
+
+    let f0 = f[0];
+
+    let width = last_idx - f0 + 1;
+
+    let mut h = vec![0; m * width];
+
+    h[..last_idx + 1 - f0].copy_from_slice(&h0[f0..last_idx + 1]);
+
+    // Possible length of consecutive chunk at each position.
+    let mut c = vec![0; m * width];
+
+    c[..last_idx + 1 - f0].copy_from_slice(&c0[f0..last_idx + 1]);
+
+    let f_sub = &mut f[1..];
+
+    let p_sub = &query[1..][..f_sub.len()];
+
+    let case_matcher = case_sensitivity.matcher(query);
+
+    for (offset, f) in f_sub.iter().enumerate() {
+        let c_len = c.len();
+        let c_ptr = c.as_mut_ptr();
+
+        let h_len = h.len();
+        let h_ptr = h.as_mut_ptr();
+
+        let pchar = p_sub.chars().nth(offset).unwrap();
+        let pidx = offset + 1;
+        let row = pidx * width;
+        let mut is_in_gap = false;
+        let t_sub = &t[*f..last_idx + 1];
+        let b_sub = &b[*f..][..t_sub.len()];
+        let c_sub = &mut c[row + f - f0..][..t_sub.len()];
+        let h_sub = &mut h[row + f - f0..][..t_sub.len()];
+
+        let c_diag = {
+            let c = unsafe { core::slice::from_raw_parts_mut(c_ptr, c_len) };
+            &mut c[row + f - f0 - 1 - width..][..t_sub.len()]
+        };
+
+        let h_diag = {
+            let h = unsafe { core::slice::from_raw_parts_mut(h_ptr, h_len) };
+            &mut h[row + f - f0 - 1 - width..][..t_sub.len()]
+        };
+
+        let h_left = {
+            let h = unsafe { core::slice::from_raw_parts_mut(h_ptr, h_len) };
+            &mut h[row + f - f0 - 1..][..t_sub.len()]
+        };
+
+        // println!("-------------");
+        // println!("pchar: {:?}", pchar);
+        // println!("pidx: {:?}", pidx);
+        // println!("row: {:?}", row);
+        // println!("inGap: {:?}", is_in_gap);
+        // println!("Tsub: {:?}", t_sub);
+        // println!("Bsub: {:?}", b_sub);
+        // println!("Csub: {:?}", c_sub);
+        // println!("Cdiag: {:?}", c_diag);
+        // println!("Hsub: {:?}", h_sub);
+        // println!("Hdiag: {:?}", h_diag);
+        // println!("Hleft: {:?}", h_left);
+        // println!("-------------");
+
+        h_left[0] = 0;
+
+        for (offset, &char) in t_sub.iter().enumerate() {
+            let col = offset + f;
+
+            let mut s1 = 0;
+
+            let mut consecutive = 0;
+
+            let s2 = if is_in_gap {
+                h_left[offset] as i32 - penalty::GAP_EXTENSION as i32
+            } else {
+                h_left[offset] as i32 - penalty::GAP_START as i32
+            };
+
+            if case_matcher.eq(pchar, char) {
+                s1 = h_diag[offset] + bonus::MATCH;
+                let mut small_b = b_sub[offset];
+                consecutive = c_diag[offset] + 1;
+
+                if consecutive > 1 {
+                    let fb = b[col - consecutive + 1];
+                    if small_b >= bonus::BOUNDARY && small_b > fb {
+                        consecutive = 1;
+                    } else {
+                        small_b = bonus::CONSECUTIVE.max(fb).max(small_b);
+                    }
+                }
+
+                if (s1 as i32) + (small_b as i32) < s2 {
+                    s1 += b_sub[offset];
+                    consecutive = 0;
+                } else {
+                    s1 += small_b;
+                }
+            }
+
+            // println!("-------------");
+            // println!("s1: {:?}", s1);
+            // println!("s2: {:?}", s2);
+            // println!("offset: {:?}", offset);
+
+            c_sub[offset] = consecutive;
+
+            is_in_gap = (s1 as i32) < s2;
+
+            let score = (s1 as i32).max(s2).max(0) as Score;
+
+            if pidx == m - 1 && score > max_score {
+                max_score = score;
+                max_score_pos = col;
+            }
+
+            // println!("-------------");
+            // println!("score: {:?}", score);
+            // println!("offset: {:?}", offset);
+
+            h_sub[offset] = score;
+        }
+    }
+
+    // println!("f0: {:?}", f0);
+    // println!("width: {:?}", width);
+    // println!("h: {:?}", h);
+    // println!("c: {:?}", c);
+    // println!("f_sub: {:?}", f_sub);
+    // println!("p_bub: {:?}", p_sub);
+
+    let j = f0;
+
+    max_score
 }
