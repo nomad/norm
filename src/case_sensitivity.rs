981f83620906fca2eed22b0adf6973e0bb8e243f
Riccardo Mazzarini <me@noib3.dev>
2023-11-19 16:41:22 +0100

    add docs to `CaseSensitivity`

--- src/case_sensitivity.rs
+++ src/case_sensitivity.rs
@@ -1,13 +1,23 @@
-/// TODO: docs
+/// Case sensitivity modes for search.
+///
+/// This defines the different types of case sensitivity that can be used when
+/// searching for the characters of a query in a candidate string.
 #[derive(Copy, Clone, Debug, Default)]
 pub enum CaseSensitivity {
-    /// TODO: docs
+    /// The search is case-sensitive. For a successful match the case of the
+    /// letters in the candidate must exactly match the case of the letters in
+    /// the query.
     Sensitive,
 
-    /// TODO: docs
+    /// The search is case-insensitive. In this mode, the case of letters is
+    /// ignored, allowing for matches regardless of whether the letters in the
+    /// query and candidate are upper or lower case.
     Insensitive,
 
-    /// TODO: docs
+    /// In this mode, the case-sensitivity of the search is determined
+    /// dynamically based on the letters of the query. If the query contains
+    /// one or more uppercase letters the search is treated as case-sensitive,
+    /// otherwise it's case-insensitive.
     #[default]
     Smart,
 }
