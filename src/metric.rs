67c51dcf3cdb4e9a7dc15e7b9ca4d37f72f06a88
Riccardo Mazzarini <me@noib3.dev>
2023-11-19 17:44:18 +0100

    add docs to `Metric`

--- src/metric.rs
+++ src/metric.rs
@@ -1,14 +1,49 @@
+use core::ops::Range;
+
 use crate::Match;
 
-/// TODO: docs.
+/// A trait representing a distance metric on strings.
+///
+/// This trait encapsulates the logic for comparing a query to a candidate
+/// string. It allows to filter out non-matches, to sort the remaining
+/// candidates based on the quality of the match, and to show which sub-strings
+/// of a candidate matched the query.
 pub trait Metric {
-    /// TODO: docs.
+    /// The type of query to be found in the candidate.
+    ///
+    /// This is generic over an associated lifetime `'a` to allow for zero-copy
+    /// parsing of the query. Metrics that don't parse queries can simply use
+    /// a `&'a str` here.
     type Query<'a>;
 
-    /// TODO: docs.
+    /// The type that expresses how closely a candidate matches the query.
+    ///
+    /// In order to behave like a distance, its [`Ord`] implementation must be
+    /// such that given two candidates `a` and `b`, it holds that
+    ///
+    /// ```
+    /// # use core::cmp::Ordering;
+    /// # let a_distance = 0;
+    /// # let b_distance = 1;
+    /// # let _ =
+    /// a_distance.cmp(&b_distance) == Ordering::Less
+    /// # ;
+    /// ```
+    ///
+    /// if and only if `a` is a better match than `b`. In other words, a lower
+    /// distance value must indicate a closer or more relevant match.
     type Distance: Copy + Ord;
 
-    /// TODO: docs.
+    /// This method calculates the "distance" between an instance of the
+    /// metric's [`Query`][Self::Query] type and a candidate string.
+    ///
+    /// A return value of `Some(distance)` means that the metric considers the
+    /// candidate to be a match for the query, with the `distance` being the
+    /// measure of how good the match is: the better the match, the lower the
+    /// distance.
+    ///
+    /// A return value of `None` means that the candidate does not match the
+    /// query and should be filtered out of the search results.
     fn distance(
         &mut self,
         query: Self::Query<'_>,
