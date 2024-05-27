bebd2b5d9987317f29cffc7487ba2aaf25307abe
Riccardo Mazzarini <me@noib3.dev>
2024-05-27 22:38:38 +0800

    fix clippy

--- tests/fzf_common.rs
+++ tests/fzf_common.rs
@@ -545,19 +545,6 @@ mod utils {
     use norm::fzf::{FzfDistance, FzfParser, FzfQuery, FzfV1, FzfV2};
     use norm::{CaseSensitivity, Metric};
 
-    /// TODO: docs
-    pub trait SortedRanges {
-        fn sorted(&self) -> Vec<Range<usize>>;
-    }
-
-    impl SortedRanges for &[Range<usize>] {
-        fn sorted(&self) -> Vec<Range<usize>> {
-            let mut sorted = self.to_vec();
-            sorted.sort_by_key(|r| r.start);
-            sorted
-        }
-    }
-
     pub trait Fzf:
         Default
         + for<'a> Metric<Query<'a> = FzfQuery<'a>, Distance = FzfDistance>
